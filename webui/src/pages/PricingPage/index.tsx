d74e2835e3e21237077117099ba487c242c5f866
chiyonn <kud0h.chi4@gmail.com>
2025-05-05 11:35:09 +0900

    update: init pricing page

---
+++ webui/src/pages/PricingPage/index.tsx
@@ -0,0 +1,11 @@
+import styles from './PricingPage.module.css';
+
+const PricingPage = () => {
+    return (
+        <div className={styles.container}>
+            Your content here
+        </div>
+    );
+};
+
+export default PricingPage;
