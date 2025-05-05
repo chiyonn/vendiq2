1ecc7db83746898077cd70c108967dddb74ac430
chiyonn <kud0h.chi4@gmail.com>
2025-05-05 13:31:04 +0900

    update: add pricing server

---
+++ pricer/cmd/server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status":"ok"}`))
+	})
+
+	http.ListenAndServe(":8080", r)
+}
