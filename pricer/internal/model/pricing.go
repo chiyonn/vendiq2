8b74e11f692da0e150cbeab50c0a40207cb22890
chiyonn <kud0h.chi4@gmail.com>
2025-05-31 21:43:23 +0900

    update: reduce fields in order to experiments

--- pricer/internal/model/pricing.go
+++ pricer/internal/model/pricing.go
@@ -6,12 +6,12 @@ import (
 
 type Pricing struct {
 	ASIN           string   `gorm:"column:asin;not null"              json:"asin"`
-	MainImageURL   string   `gorm:"column:main_image_url;not null"    json:"mainImageUrl"`
+	//MainImageURL   string   `gorm:"column:main_image_url;not null"    json:"mainImageUrl"`
 	MinPrice       *float64 `gorm:"column:min_price"                  json:"minPrice,omitempty"`
 	MaxPrice       *float64 `gorm:"column:max_price"                  json:"maxPrice,omitempty"`
-	NumOfSellers   int      `gorm:"column:num_of_sellers;not null"    json:"numOfSellers"`
-	BuyboxPrice    float64  `gorm:"column:buybox_price;not null"      json:"buyboxPrice"`
-	BuyboxSellerID string   `gorm:"column:buybox_seller_id;not null"  json:"buyboxSellerId"`
+	//NumOfSellers   int      `gorm:"column:num_of_sellers;not null"    json:"numOfSellers"`
+	//BuyboxPrice    float64  `gorm:"column:buybox_price;not null"      json:"buyboxPrice"`
+	//BuyboxSellerID string   `gorm:"column:buybox_seller_id;not null"  json:"buyboxSellerId"`
 	AutoPricing    bool     `gorm:"column:auto_pricing;not null;default:false" json:"autoPricing"`
 
 	CreatedAt time.Time  `gorm:"column:created_at;not null" json:"createdAt"`
